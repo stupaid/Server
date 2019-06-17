8326cad520f9087fc967cc54ac0fb361586e9514
mj163 <mengjianzuishuai@qq.com>
2019-06-17 17:04:16 +0800

    add so.py

---
+++ so.py
@@ -0,0 +1,25 @@
+var01 = "python is good language"
+# p  y  t  h  o  n     i  s     g  o  o  d     l  a  n  g  u  a  g  e
+# 0  1  2  3  4  5  6  7  8  9  10 11 12 13 14 15 16 17 18 19 20 21 22
+
+# print(len(var01))
+# print(var01[0:3])
+# print(var01[0:])
+# print(var01[:-1])
+#
+# for index in range(len(var01)):
+#   print(var01[index])
+#
+# for i in var01:
+#     print(i)
+
+var01_split = var01.split()
+print(var01_split, type(var01_split))
+
+var01_replace = var01.replace('o', '0')
+print(var01_replace)
+
+if "o" in var01:
+    print("o exist in var01")
+else:
+    print("o not exist in var01")
